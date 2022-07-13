397dff80383807443ad4610ea158d2bab62acbce
yinmaw672022 <yinmawlin@za.com.mm>
2022-07-13 13:42:30 +0630

    update one

--- register.php
+++ register.php
@@ -4,9 +4,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <!-- <link rel="stylesheet" href="css/bootstrap.min.css"> -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
-    
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
     <title>Register</title>
 
     <style>
